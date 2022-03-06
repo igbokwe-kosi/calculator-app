391ba929587c7efb56781a9c626d173d247d3e6e
igbokwe-kosi <igbokwekosiso@gmail.com>
2022-03-06 20:20:39 +0100

    Added feature

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 'use strict';
 
+//
+// ──────────── SELECTORS ────────────────────────────────────────────────────────
+//
 const body = document.body;
 const toggleTheme = document.querySelector('.toggle-theme__button');
+const btnEls = document.querySelectorAll('button');
+const btnNumber = document.querySelectorAll('.buttons__number');
+const btnOperator = document.querySelectorAll(
+  '.buttons__operator[data-operator]'
+);
+const btnEqual = document.querySelector('.buttons__operator[data-equal]');
+const btnDelete = document.querySelector('.buttons__delete');
+const btnReset = document.querySelector('.buttons__reset');
+
+//
+// ──────────── THEME AND GENERAL BUTTON FUNCTIONALITY ────────────────────────────────────────────────────────
+//
 let toggle;
+
+btnEls.forEach(btn =>
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    btn.classList.add('pressed');
+    btn.addEventListener('transitionend', () =>
+      btn.classList.remove('pressed')
+    );
+  })
+);
 
 toggleTheme.addEventListener('click', function (e) {
   [...this.children].forEach(child => child.classList.remove('active'));
@@ -12,6 +37,10 @@ toggleTheme.addEventListener('click', function (e) {
   body.className = '';
   body.classList.add(`theme-${toggle}`);
 });
+
+//
+// ──────────── CALCULATOR FUNCTION ────────────────────────────────────────────────────────
+//
 
 class Calculator {
   constructor() {}
