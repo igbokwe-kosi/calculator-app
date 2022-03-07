dc2ca829f70a34497545b9ad7adca8087ceacff3
igbokwe-kosi <igbokwekosiso@gmail.com>
2022-03-07 14:22:54 +0100

    App functions

--- script.js
+++ script.js
@@ -14,6 +14,13 @@ const btnEqual = document.querySelector('.buttons__operator[data-equal]');
 const btnDelete = document.querySelector('.buttons__delete');
 const btnReset = document.querySelector('.buttons__reset');
 
+const previousOperandTextElement = document.querySelector(
+  '[data-previous-operand]'
+);
+const currentOperandTextElement = document.querySelector(
+  '[data-current-operand]'
+);
+
 //
 // ──────────── THEME AND GENERAL BUTTON FUNCTIONALITY ────────────────────────────────────────────────────────
 //
@@ -43,6 +50,91 @@ toggleTheme.addEventListener('click', function (e) {
 //
 
 class Calculator {
-  constructor() {}
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+  appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+    console.log(this.currentOperand);
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === '') return;
+    if (this.previousOperand !== '') {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = '';
+  }
+  compute() {
+    console.log(`computing`);
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) console.log(prev, current);
+    switch (this.operation) {
+      case '+':
+        computation = prev + current;
+        break;
+      case '-':
+        computation = prev - current;
+        break;
+      case '*':
+        computation = prev * current;
+        break;
+      case '/':
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = '';
+    console.log(computation);
+  }
+  updateDisplay() {
+    this.currentOperandTextElement.textContent = this.currentOperand;
+  }
+  clear() {
+    this.previousOperand = '';
+    this.currentOperand = '';
+    this.operation = undefined;
+  }
 }
-const calculator = new Calculator();
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+btnDelete.addEventListener('click', calculator.delete);
+btnEqual.addEventListener('click', () => {
+  // console.log('clicked');
+  calculator.compute();
+  calculator.updateDisplay();
+});
+btnNumber.forEach(btn =>
+  btn.addEventListener('click', () => {
+    calculator.appendNumber(btn.innerText);
+    calculator.updateDisplay();
+  })
+);
+btnReset.addEventListener('click', () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+btnOperator.forEach(btn =>
+  btn.addEventListener('click', e => {
+    calculator.chooseOperation(btn.innerText);
+    calculator.updateDisplay();
+  })
+);
